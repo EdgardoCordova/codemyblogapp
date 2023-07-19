c0455353484cd1032a1481f3bf05c0c7dd72ad63
Edgardo Cordova <cordovabruch@gmail.com>
2023-07-19 15:41:11 -0600

    commit luego de likes

--- theblog/views.py
+++ theblog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm, EditarForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -31,7 +32,15 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 class AddPostView(CreateView):
@@ -65,4 +74,15 @@ def CategoryListView(request):
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-',' '))
     return render(request, 'categories.html', {'cats': cats.title().replace('-',' '), 'category_posts': category_posts})
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
